c914919e78bcb280f0ba72a9dbb09e7bd62243ca
Manfred Touron <m@42.am>
2014-12-15 23:02:35 +0100

    curl is working

--- go-dl-extract.go
+++ go-dl-extract.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,21 +14,19 @@ func main() {
 	flag.Parse()
 	fmt.Printf("url: %s\n", url)
 
-	/*
-		resp, err := http.Get(url)
-		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
-		f, err := os.Create("my.tar.gz")
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	f, err := os.Create("my.tar.gz")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-		_, err = io.Copy(f, resp.Body)
-		if err != nil {
-			panic(err)
-		}
-	*/
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		panic(err)
+	}
 }
