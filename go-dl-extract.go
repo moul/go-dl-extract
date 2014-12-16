faf11e509ec3c39a6299e658a5ca140397c4c53b
Manfred Touron <m@42.am>
2014-12-16 21:08:17 +0100

    wip

--- go-dl-extract.go
+++ go-dl-extract.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -56,24 +57,47 @@ func init() {
 //   RUN ADD {URL} /
 //   RUN -v -url={URL}
 func main() {
+	// Download
 	log.Debugf("Downloading '%s' to '%s'", url, tmptar)
 	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
+
 	f, err := os.Create(tmptar)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
+
 	_, err = io.Copy(f, resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
+	// Extract
 	log.Debugf("Extracting '%s' to '%s'", tmptar, dest)
 	nf, err := os.Open(tmptar)
+	if err != nil {
+		panic(err)
+	}
 	defer nf.Close()
-	archive.Untar(nf, dest, nil)
+
+	fi, err := os.Stat("/bin/sh")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(fi.Size())
+
+	err2 := archive.Untar(nf, dest, nil)
+	if err2 != nil {
+		panic(err2)
+	}
+
+	fi2, err := os.Stat("/bin/sh")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(fi2.Size())
 }
