748289dc5bf151cd058dda55cd14f9ebebb0d119
Manfred Touron <m@42.am>
2014-12-16 20:38:41 +0100

    Setup logging + splitted main and init

--- go-dl-extract.go
+++ go-dl-extract.go
@@ -2,66 +2,78 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"strings"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/pkg/archive"
 	_ "github.com/docker/docker/pkg/units" // Required for godep
 )
+
+var url string
+var verbose bool
+var dest string
+var tmptar string
+var is_bin_sh string
+
+func init() {
+	// flags
+	flag.StringVar(&is_bin_sh, "c", "", "Is $0 /bin/sh ?")
+	flag.Parse()
+
+	flag.StringVar(&url, "url", "", "help message for flagname")
+	flag.BoolVar(&verbose, "v", false, "Verbose")
+	flag.StringVar(&dest, "dest", "/", "Destination path")
+	tmptar = "/tmp.tar"
+
+	flag.CommandLine.Parse(strings.Split(is_bin_sh, " "))
+	if len(flag.Args()) > 0 && len(url) == 0 {
+		url = flag.Args()[0]
+	}
+
+	// logging
+	log.SetOutput(os.Stderr)
+	//log.SetFormatter(log.TextFormatter)
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.WarnLevel)
+	}
+
+	log.WithFields(log.Fields{
+		"verbose": verbose,
+		"url":     url,
+		"dest":    dest,
+		"tail":    flag.Args(),
+	}).Debug("Args")
+
+}
 
 // Usage in a Dockerfile:
 //   RUN {URL}
 //   RUN ADD {URL} /
 //   RUN -v -url={URL}
 func main() {
-	// Parsing
-	is_bin_sh := flag.String("c", "", "Is $0 /bin/sh ?")
-	flag.Parse()
-
-	url := flag.String("url", "", "help message for flagname")
-	verbose := flag.Bool("v", false, "Verbose")
-	dest := flag.String("dest", "/", "Destination path")
-	tmptar := "tmp.tar"
-
-	flag.CommandLine.Parse(strings.Split(*is_bin_sh, " "))
-	if len(flag.Args()) > 0 && len(*url) == 0 {
-		url = &flag.Args()[0]
-	}
-
-	if *verbose {
-		fmt.Println("verbose:", *verbose)
-		fmt.Println("url:", *url)
-		fmt.Println("dest:", *dest)
-		fmt.Println("tail:", flag.Args())
-	}
-
-	// Get
-	resp, err := http.Get(*url)
+	log.Debugf("Downloading '%s' to '%s'", url, tmptar)
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-
-	// Create the tarball locally
 	f, err := os.Create(tmptar)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-
-	// Fill the tarball
 	_, err = io.Copy(f, resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	// Open the tarball
+	log.Debugf("Extracting '%s' to '%s'", tmptar, dest)
 	nf, err := os.Open(tmptar)
 	defer nf.Close()
-
-	// Extract the tarball
-	archive.Untar(nf, *dest, nil)
+	archive.Untar(nf, dest, nil)
 }
